b02a7c8dcd510de15cd856547d6026fe6b409cef
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2022-09-11 21:00:10 +0700

    Add files via upload

---
+++ web-php5/project/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Blog Home - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <?php 
+            require_once "pages/nav.php";
+        ?>
+        <!-- Page header with logo and tagline-->
+        
+        <!-- Page content-->
+        <?php 
+            require_once "pages/content.php";
+        ?>
+        <!-- Footer-->
+            <?php 
+                require_once "pages/footer.php";
+            ?>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
