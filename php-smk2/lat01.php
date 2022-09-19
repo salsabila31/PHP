3113449b99ccfe7abe300a749c9e7e607121f8c4
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2022-09-19 19:24:33 +0700

    Add files via upload

---
+++ php-smk2/lat01.php
@@ -0,0 +1,14 @@
+<?php 
+    echo "Belajar PHP";
+    echo '<br>';
+    echo 'Saya siswa SMK';
+
+    echo '<h1 style="background-color:red">Belajar PHP itu Mudah</h1>
+          <p style="background-color:yellow"> Saya Suka Belajar Program </p>
+    ';
+
+    echo 'Saya Belajar' . ' PHP' . '<br>';
+    
+    echo 2019 + 2;
+?>
+
