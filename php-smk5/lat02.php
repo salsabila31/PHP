695b9864028a5cfe072e0e16d8f9d33caa44b0e0
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2022-09-21 08:20:36 +0700

    Add files via upload

---
+++ php-smk5/lat02.php
@@ -0,0 +1,13 @@
+<?php 
+    $tulisan = 'Saya Belajar PHP';
+    echo $tulisan . '<br>';
+
+    $angka = 2019;
+    echo 'Tahun ' . $angka . '<br>';
+
+    var_dump($tulisan);
+
+    echo '<br>';
+
+    var_dump((int)$angka+2);
+?>
