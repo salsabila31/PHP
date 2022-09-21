9c7984496aafa1b3fe396a5307ed203e6bf4e523
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2022-09-21 19:15:29 +0700

    Add files via upload

---
+++ php-smk6/lat05.php
@@ -0,0 +1,45 @@
+<?php 
+    // $tanggal = 20;
+
+    // // $hasil = $tanggal < 31;
+
+    // if ($tanggal < 31) {
+    //     if ($tanggal > 0) {
+    //         echo 'Benar';
+    //     }
+    //     else {
+    //         echo 'Salah';
+    //     }
+    // }
+    // else {
+    //     echo 'Salah';
+    // }
+
+    $nilai = 101;
+
+    // if ($nilai <= 100) {
+    //     if ($nilai >= 0) {
+    //         echo 'Nilai Benar';
+    //     }
+    //     else {
+    //         echo 'Nilai Salah';
+    //     }
+    // }
+    // else {
+    //     echo 'Nilai salah';
+    // }
+
+    // if ($nilai >= 0 && $nilai <=100) {
+    //     echo 'Nilai Benar';
+    // }
+    // else {
+    //     echo 'Nilai Salah';
+    // }
+
+        if ($nilai >=100 || $nilai <=0) {
+           echo 'Nilai Salah';
+        }
+        else {
+            echo 'Nilai Benar';
+        }
+?>
