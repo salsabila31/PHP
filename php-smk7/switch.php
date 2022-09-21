358fc0f35c5e224cee451eab6cf133ef3868140c
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2022-09-21 19:16:24 +0700

    Add files via upload

---
+++ php-smk7/switch.php
@@ -0,0 +1,41 @@
+<?php 
+    // $hari = 4;
+
+    // switch ($hari) {
+    //     case 1:
+    //         echo 'Minggu';
+    //         break;
+
+    //     case 2:
+    //         echo 'Senin';
+    //         break;
+
+    //     case 3:
+    //         echo 'Selasa';
+    //         break;
+        
+    //     default:
+    //         echo 'Hari belum dibuat';
+    //         break;
+    // }
+
+    $pilihan = 'simpan';
+
+    switch ($pilihan) {
+        case 'tambah':
+            echo 'anda memilih tambah';
+            break;
+        
+        case 'ubah':
+            echo 'anda memilih ubah';
+            break;
+
+        case 'hapus':
+            echo 'anda memilih hapus';
+            break;
+
+        default:
+            echo 'pilihan belum ada';
+            break;
+    }
+?>
