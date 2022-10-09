07287f4dcb541287162964a8f8b742971fba8e56
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2022-10-09 21:46:26 +0700

    Add files via upload

---
+++ php-smk20/restoran/kategori/select.php
@@ -0,0 +1,68 @@
+<div style="margin:auto; width:900px;">
+
+<h3><a href="http://localhost/php-smk20/restoran/kategori/insert.php">TAMBAH DATA</a></h3>
+
+<?php 
+    require_once "../function.php";
+
+    $sql = "SELECT idkategori FROM tblkategori";
+    $result = mysqli_query($koneksi,$sql);
+
+    $jumlahdata = mysqli_num_rows($result);
+
+    $banyak = 3;
+
+    
+    $halaman = ceil($jumlahdata / $banyak);
+
+    for ($i=1; $i < $halaman; $i++) { 
+        echo '<a href="?p='.$i.'">'.$i.'</a>';
+        echo '&nbsp &nbsp &nbsp';
+    }
+
+    echo '<br> <br>';   
+    
+    if (isset($_GET['p'])) {
+        $p=$_GET['p'];
+       $mulai = ($p * $banyak) -$banyak;
+       //   6 = (3 * 3) - 3
+    }
+    else {
+        $mulai = 0;
+    }
+
+    $sql = "SELECT * FROM tblkategori LIMIT $mulai,$banyak";
+
+    $result = mysqli_query($koneksi,$sql);
+
+    // var_dump($result);
+
+    $jumlah = mysqli_num_rows($result);
+    // echo '<br>';
+    // echo $jumlah;
+
+   echo '
+   <table border="1px">
+   <tr>
+    <th>No</th>
+    <th>Kategori</th>
+   </tr>
+    ';
+
+    $no=$mulai+1;
+
+    if ($jumlah > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<tr>';
+            echo '<td>'.$row['idkategori'].'</td>';
+            echo '<td>'.$row['kategori'].'</td>';
+            echo '</tr>';
+
+        }
+    }
+
+    echo '</table>';
+?>
+
+</div>
+
