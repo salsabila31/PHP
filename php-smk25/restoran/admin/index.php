d374e9f8559756b7841db8d28e1edc3089196338
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2022-10-19 21:14:59 +0700

    Add files via upload

---
+++ php-smk25/restoran/admin/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page | Aplikasi Restoran SMK</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-3">
+                <h2>Restoran</h2>
+            </div>
+
+            <div class="col-md-9">
+                <div class="float-end mt-4">logout</div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item"><a class="nav-link" href="#">Kategori</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Pelanggan</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Order</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Order Detail</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">User</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-9"></div>
+        </div>
+
+        <div class="row mt-5">
+
+            <div class="col">
+                <p class="text-center">2019 - copyright@smkrevit.com</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
