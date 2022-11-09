a4283423e4254d4cd301e5e9f650a551641507fd
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2022-11-09 07:56:07 +0700

    Add files via upload

---
+++ php-smk36/restoran/admin/login.php
@@ -0,0 +1,68 @@
+<?php 
+    session_start();
+    require_once "../dbcontroller.php";
+    $db = new DB;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Restoran</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-4 mx-auto mt-4">
+            <div>
+                <form action="" method="post">
+                    <div>
+                        <h3>LOGIN RESTORAN</h3>
+                    </div>
+                    <div class="form-group">
+                        <label for="">EMAIL</label>
+                        <input type="email" name="email" required class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="">PASSWORD</label>
+                        <input type="password" name="password" required class="form-control">
+                    </div>
+
+                <div>
+                    <input type="submit" name="login" value="LOGIN" class="btn btn-primary">
+                </div>
+                </form>
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php 
+    if (isset($_POST['login'])) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT * FROM tbluser WHERE email='$email' AND '$password' ";
+
+        $count = $db->rowCOUNT($sql);
+
+        if ($count == 0) {
+            echo "<h3>Email atau Password salah</h3>";
+        }
+        else {
+            $sql = "SELECT * FROM tbluser WHERE email='$email' AND '$password' ";
+            $row = $db->getITEM($sql);
+
+            $_SESSION['user']=$row['email'];
+            $_SESSION['level']=$row['level'];
+
+            header("location:index.php");
+        }
+    }
+?>
