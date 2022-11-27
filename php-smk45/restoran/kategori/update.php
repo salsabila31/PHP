43d20e2c7cb3e407b3490774b67151c102fc7374
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2022-11-27 20:11:44 +0700

    Add files via upload

---
+++ php-smk45/restoran/kategori/update.php
@@ -0,0 +1,36 @@
+<?php 
+    if (isset($_GET['id'])) {
+        $id=$_GET['id'];
+
+        $sql = "SELECT * FROM tblkategori WHERE idkategori=$id";
+
+        $row=$db->getITEM($sql);
+    }
+?>
+
+<h3>Update kategori</h3>
+
+<div>
+    <form action="" method="post">
+        <div class="form-group w-50">
+            <label for="">Nama Kategori</label>
+            <input type="text" name="kategori" required value="<?php echo $row['kategori']?>" class="form-control">
+        </div>
+
+        <div>
+            <input type="submit" name="simpan" value="simpan" class="btn btn-secondary">
+        </div>
+    </form>
+</div>
+
+<?php 
+    if (isset($_POST['simpan'])) {
+        $kategori = $_POST['kategori'];
+
+        $sql= "UPDATE tblkategori SET kategori='$kategori' WHERE idkategori=$id";
+    
+        $db->runSQL($sql);
+
+        header("location:?f=kategori&m=select");
+    }
+?>
